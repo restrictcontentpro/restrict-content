400d2fb0684ff28070dad7c885ae50f252789fd0
Ashley Evans <ashley@nosegraze.com>
2017-04-27 16:09:07 +0100

    WIP: Simplify access/user checks. #10

--- includes/user-checks.php
+++ includes/user-checks.php
@@ -12,6 +12,8 @@
  * Add a different filter to the post content based on
  * the current user's capabilities.
  *
+ * @deprecated 2.1.4 Handled by rc_user_can_access() and rc_filter_restricted_content() instead.
+ *
  * @return void
  */
 function rcCheckUser() {
@@ -38,4 +40,33 @@ function rcCheckUser() {
 	}
 }
 
-add_action( 'loop_start', 'rcCheckUser' );
+//add_action( 'loop_start', 'rcCheckUser' );
+
+/**
+ * Checks whether a user can access a post
+ *
+ * @param int $user_id ID of the user to check, or 0 for the current user.
+ * @param int $post_id ID of the post to check, or 0 for the current post.
+ *
+ * @since 2.1.4
+ * @return bool Whether or not the user has access to view the post.
+ */
+function rc_user_can_access( $user_id = 0, $post_id = 0 ) {
+
+	if ( empty( $user_id ) ) {
+		$user_id = get_current_user_id();
+	}
+
+	if ( empty( $post_id ) ) {
+		$post_id = get_the_ID();
+	}
+
+	$required_level = get_post_meta( $post_id, 'rcUserLevel', true );
+
+	if ( empty( $required_level ) || 'None' == $required_level || current_user_can( 'manage_options' ) ) {
+		return true;
+	}
+
+	return user_can( $user_id, strtolower( $required_level ) );
+
+}
